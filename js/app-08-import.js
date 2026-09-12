3d1364227e9ce454ca91db3dc448a509790a95d9
dfwvhfnw7y-coder <stanislav.pavlacek@suw.cz>
2026-09-12 20:10:07 +0200

    chore(v43): load event selector module

--- js/app-08-import.js
+++ js/app-08-import.js
@@ -117,3 +117,6 @@ function runCustomerImport(){
   }
   Promise.all(jobs).then(function(){flash('📥 Importováno '+todo.length+' zákazníků');closeCustomerImport();if(!fbReady)loadCustomersREST()}).catch(function(err){flash('Chyba importu: '+err.message,true)});
 }
+
+/* v43: load event selector as a small independent module */
+(function(){var s=document.createElement('script');s.src='js/app-09-events-ui.js';document.body.appendChild(s)})();
