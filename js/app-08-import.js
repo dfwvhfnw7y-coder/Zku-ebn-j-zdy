2046f3ccec9849fb400bd46e200f474a0bf4c10c
dfwvhfnw7y-coder <stanislav.pavlacek@suw.cz>
2026-09-13 11:52:28 +0200

    Load V45 event module with release cache key

--- js/app-08-import.js
+++ js/app-08-import.js
@@ -118,5 +118,5 @@ function runCustomerImport(){
   Promise.all(jobs).then(function(){flash('📥 Importováno '+todo.length+' zákazníků');closeCustomerImport();if(!fbReady)loadCustomersREST()}).catch(function(err){flash('Chyba importu: '+err.message,true)});
 }
 
-/* v44: load event selector with release cache key */
-(function(){var s=document.createElement('script');s.src='js/app-09-events-ui.js?v=44';document.body.appendChild(s)})();
+/* v45: load event selector with release cache key */
+(function(){var s=document.createElement('script');s.src='js/app-09-events-ui.js?v=45';document.body.appendChild(s)})();
